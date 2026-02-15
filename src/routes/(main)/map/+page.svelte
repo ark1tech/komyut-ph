5763484cdcbf656d2f69c418d4df759efd0a22b0
RK Mañago <rbmanago1@up.edu.ph>
2026-02-15 13:47:25 +0800

    chore: explore shinanegans

--- src/routes/(main)/map/+page.svelte
+++ src/routes/(main)/map/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import Map from '$lib/components/map/Map.svelte';
 	import MapSearchBar from '$lib/components/map/MapSearchBar.svelte';
+	import * as Drawer from "$lib/components/ui/drawer/index";
+	import * as Button from "$lib/components/ui/button/index";
 </script>
 
 <svelte:head>
@@ -11,4 +13,17 @@
 <div class="relative flex-1">
 	<Map />
 	<MapSearchBar />
+	<Drawer.Root>
+		<Drawer.Trigger>Open</Drawer.Trigger>
+		<Drawer.Content>
+			<Drawer.Header>
+				<Drawer.Title>Are you sure absolutely sure?</Drawer.Title>
+				<Drawer.Description>This action cannot be undone.</Drawer.Description>
+			</Drawer.Header>
+			<Drawer.Footer>
+				<Button>Submit</Button>
+				<Drawer.Close>Cancel</Drawer.Close>
+			</Drawer.Footer>
+		</Drawer.Content>
+	</Drawer.Root>
 </div>
